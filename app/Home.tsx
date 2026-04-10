8d3ca22e337721fa70bdeb2e98db17a09b78133c
Jokkim <jokkimxavier@gmail.com>
2026-04-11 00:34:19 +0530

    secrured the key

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,8 +1,34 @@
 import { View,Text } from "react-native";
 import { db,auth } from "@/lib/firebase";
-import { collection,query,where,onSnapshot,or } from "firebase/firestore";
+import { collection,query,where,onSnapshot,or, orderBy } from "firebase/firestore";
+import { useState,useEffect } from "react";
+
 
 export default function HomeScreen () {
+    const[tasks,setTasks]=useState([]);
+
+    useEffect(() => {
+        const user = auth.currentUser;
+        if(!user) return;
+
+        const q = query(
+            collection(db,'tasks'),
+            where('userId','==',user.uid),
+            orderBy('dueDate','asc')
+        );
+
+        const unSubscribe = onSnapshot(q,(snapshot) => {
+            const taskList = snapshot.docs.map(doc => ({
+                id:doc.id,
+                ...doc.data()
+            }));
+            setTasks(taskList);
+            console.log(tasks)
+        })
+
+        return () => unSubscribe();
+
+    },[])
     return (
         <View >
             <Text>HELLO WORLD</Text>
